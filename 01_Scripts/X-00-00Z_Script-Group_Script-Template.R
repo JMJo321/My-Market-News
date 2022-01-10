7d018a40c3869192bffc23e27c436b84ae5bf52b
JMJo321 <jinmahn.jo@gmail.com>
2022-01-09 23:45:57 -0800

    Add a template script for R

---
+++ 01_Scripts/X-00-00Z_Script-Group_Script-Template.R
@@ -0,0 +1,53 @@
+# < Description > *
+# > Script Group Indicator Number and Name
+# # : X-00, XYZ
+# #
+# > Script Number(s)
+# # : X-00-000
+# #
+# > Purpose of the script(s)
+# # : (...)
+
+# ------------------------------------------------------------------------------
+# Load required libraries
+# ------------------------------------------------------------------------------
+# library()
+
+
+# ------------------------------------------------------------------------------
+# Set working directory, and run header script
+# ------------------------------------------------------------------------------
+# ------- Set project name -------
+PROJ.NAME <- "My-Market-News"
+
+
+# ------- Set working directory -------
+PATH_PROJ <-
+  paste("/Users/jmjo/Dropbox/00_JMJo/Projects", PROJ.NAME, sep = "/")
+setwd(PATH_PROJ)
+
+
+# ------- Run the header script -------
+PATH_HEADER <- paste0("05_Code/H-", PROJ.NAME, ".R")
+source(PATH_HEADER)
+
+
+# ------------------------------------------------------------------------------
+# Define path(s), parameter(s) and function(s)
+# ------------------------------------------------------------------------------
+# ------- Define path(s) -------
+# (Not Applicable)
+
+
+# ------- Define parameter(s) -------
+# (Not Applicable)
+
+
+# ------- Define function(s) -------
+# (Not Applicable)
+
+
+# ------------------------------------------------------------------------------
+# (...)
+# ------------------------------------------------------------------------------
+# ------- (...) -------
