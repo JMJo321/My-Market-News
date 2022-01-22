79341e32cf86e2782f2fd7fa025321fa618183d2
JMJo321 <jinmahn.jo@gmail.com>
2022-01-21 21:36:11 -0800

    Add a data-dictionary-related script (#1)

---
+++ 01_Scripts/D-My-Market-News.R
@@ -0,0 +1,103 @@
+# < Description > *
+# > Script Group Indicator Number and Name
+# # : D, MMN
+# #
+# > Script Number(s)
+# # : (Not Applicable)
+# #
+# > Purpose of the script(s)
+# # : Data Dictionary
+
+# ------------------------------------------------------------------------------
+# Load required libraries
+# ------------------------------------------------------------------------------
+# (Not Applicable)
+
+
+# ------------------------------------------------------------------------------
+# Set working directory, and run header script
+# ------------------------------------------------------------------------------
+# ------- Set project name -------
+# (Not Applicable)
+
+
+# ------- Set working directory -------
+# (Not Applicable)
+
+
+# ------- Run the header script -------
+# (Not Applicable)
+
+
+# ------------------------------------------------------------------------------
+# Define path(s), parameter(s) and function(s)
+# ------------------------------------------------------------------------------
+# ------- Define path(s) -------
+# (Not Applicable)
+
+
+# ------- Define parameter(s) -------
+# (Not Applicable)
+
+
+# ------- Define function(s) -------
+# (Not Applicable)
+
+
+# ------------------------------------------------------------------------------
+# Data Dictionary
+# ------------------------------------------------------------------------------
+# ------- Creat a list for renaming variables -------
+list_change.var.names <- list(
+  # (Old Name) = (New Name)
+  average_price = "avg_price",
+  avgleaf = "avg_leaf",
+  avglength = "avg_length",
+  avgloanpd = "avg_loan_premiums_and_discounts",
+  avgmike = "avg_mike",
+  avgstaple = "avg_staple",
+  avgstrength = "avg_strength",
+  avgtrash = "avg_trash",
+  avguniformity = "avg_uniformity",
+  bales = "bale_count",
+  bales_count = "bale_count",
+  begin_date = "begin_date",
+  `beginning date` = "begin_date",
+  `color grade` = "color_grade",
+  `crop year` = "crop_year",
+  currentmth_1stday = "current_month_1stday",
+  eligible_percent = "eligible_pct",
+  enddate = "end_date",
+  `end date` = "end_date",
+  `ending date` = "end_date",
+  holdings_change_percent = "holdings_change_pct",
+  holdings_1stdaymth_lbs = "holdings_1stdaymonth_lbs",
+  `leaf grade` = "leaf_grade",
+  max_price = "price_max",
+  min_price = "price_min",
+  `monthly avg` = "monthly_avg",
+  `non-conventional` = "non_conventional",
+  number = "publish_number",
+  numberofbales = "bale_count",
+  numberofgins = "gin_count",
+  pkg = "package",
+  percentage_change = "pct_change",
+  percent = "pct",
+  pctbark = "pct_bark",
+  pctgrass = "pct_grass",
+  pcttenderable = "pct_tenderable",
+  pct_chg_lastmonth = "pct_change_last_month",
+  pct_chg_lastyear = "pct_change_last_year",
+  pct_chg_last_week = "pct_change_last_week",
+  predominatecolor = "predominate_color",
+  predominatecolorpct = "predominate_color_pct",
+  predominateleaf = "predominate_leaf",
+  predominateleafpct = "predominate_leaf_pct",
+  `price_change (%)` = "price_change_pct",
+  startdate = "begin_date",
+  tenderable_percent = "tenderable_pct",
+  volume = "publish_volume",
+  `weekly av` = "weekly_avg",
+  `yearly avg` = "yearly_avg",
+  `% breakdown` = "breakdown"
+)
